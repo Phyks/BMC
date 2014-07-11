7e570322c06496352ac0435bfff64a6b0425aba8
François Boulogne <fboulogne sciunto org>
2014-07-10 22:56:47 -0400

    fix paths

--- libbmc/tests/test_fetcher.py
+++ libbmc/tests/test_fetcher.py
@@ -15,11 +15,11 @@ from libbmc.fetcher import *
 
 class TestFetcher(unittest.TestCase):
     def setUp(self):
-        with open("tests/src/doi.bib", 'r') as fh:
+        with open("libbmc/tests/src/doi.bib", 'r') as fh:
             self.doi_bib = fh.read()
-        with open("tests/src/arxiv.bib", 'r') as fh:
+        with open("libbmc/tests/src/arxiv.bib", 'r') as fh:
             self.arxiv_bib = fh.read()
-        with open("tests/src/isbn.bib", 'r') as fh:
+        with open("libbmc/tests/src/isbn.bib", 'r') as fh:
             self.isbn_bib = fh.read()
 
     def test_download(self):
@@ -35,13 +35,13 @@ class TestFetcher(unittest.TestCase):
 
     def test_findISBN_DJVU(self):
         # ISBN is incomplete in this test because my djvu file is bad
-        self.assertEqual(findISBN("tests/src/test_book.djvu"), '978295391873')
+        self.assertEqual(findISBN("libbmc/tests/src/test_book.djvu"), '978295391873')
 
     def test_findISBN_PDF(self):
-        self.assertEqual(findISBN("tests/src/test_book.pdf"), '9782953918731')
+        self.assertEqual(findISBN("libbmc/tests/src/test_book.pdf"), '9782953918731')
 
     def test_findISBN_False(self):
-        self.assertFalse(findISBN("tests/src/test.pdf"))
+        self.assertFalse(findISBN("libbmc/tests/src/test.pdf"))
 
     def test_isbn2Bib(self):
         self.assertEqual(isbn2Bib('0198507194'), self.isbn_bib)
@@ -50,16 +50,16 @@ class TestFetcher(unittest.TestCase):
         self.assertEqual(isbn2Bib('foo'), '')
 
     def test_findDOI_PDF(self):
-        self.assertEqual(findDOI("tests/src/test.pdf"),
+        self.assertEqual(findDOI("libbmc/tests/src/test.pdf"),
                          "10.1103/physrevlett.112.253201")
 
     def test_findDOI_DJVU(self):
         # DOI is incomplete in this test because my djvu file is bad
-        self.assertEqual(findDOI("tests/src/test.djvu"),
+        self.assertEqual(findDOI("libbmc/tests/src/test.djvu"),
                          "10.1103/physrevlett.112")
 
     def test_findDOI_False(self):
-        self.assertFalse(findDOI("tests/src/test_arxiv_multi.pdf"))
+        self.assertFalse(findDOI("libbmc/tests/src/test_arxiv_multi.pdf"))
 
     def test_doi2Bib(self):
         self.assertEqual(doi2Bib('10.1103/physreva.88.043630'), self.doi_bib)
@@ -68,7 +68,7 @@ class TestFetcher(unittest.TestCase):
         self.assertEqual(doi2Bib('blabla'), '')
 
     def test_findArXivId(self):
-        self.assertEqual(findArXivId("tests/src/test_arxiv_multi.pdf"),
+        self.assertEqual(findArXivId("libbmc/tests/src/test_arxiv_multi.pdf"),
                          '1303.3130v1')
 
     def test_arXiv2Bib(self):
@@ -78,7 +78,7 @@ class TestFetcher(unittest.TestCase):
         self.assertEqual(arXiv2Bib('blabla'), '')
 
     def test_findHALId(self):
-        self.assertTupleEqual(findHALId("tests/src/test_hal.pdf"),
+        self.assertTupleEqual(findHALId("libbmc/tests/src/test_hal.pdf"),
                               ('hal-00750893', '3'))
 
 if __name__ == '__main__':
